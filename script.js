8bf83a059875c98f5008d72481b61375bd7b994f
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-08-29 00:29:05 +0600

    Added button ripple efect

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function loadMore(){
         imgCont.appendChild(newImg);
     }
 }
+
+// button ripple effect
+button.addEventListener('mouseover', (event)=>{
+    //console.log(event.pageX - button.offsetLeft);
+    //pageX = from page on x axis far left and offsetLeft means from left side element distance
+    let x = event.pageX - button.offsetLeft;
+    let y = event.pageY - button.offsetTop;
+    button.style.setProperty('--xPos', x + 'px');
+    button.style.setProperty('--yPos', y + 'px');
+});
